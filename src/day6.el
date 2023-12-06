0d501bae95875029f37d18070742d1ef8da657fe
Graham Marlow <graham@onesignal.com>
2023-12-06 11:43:23 -0800

    Day 6

---
+++ src/day6.el
@@ -0,0 +1,26 @@
+(defun distance (held max)
+  (* (- max held) held))
+
+(defun expand (n)
+  (let (rst)
+    (dotimes (i (+ n 1))
+      (push i rst))
+    rst))
+
+(defun distances (time)
+  (mapcar (lambda (i) (distance i time)) (expand time)))
+
+(defun distance-prs (time record)
+  (seq-filter (lambda (d) (> d record)) (distances time)))
+
+(defun num-prs (time record)
+  (length (distance-prs time record)))
+
+;; Part 1
+(* (num-prs 42 284)
+   (num-prs 68 1005)
+   (num-prs 69 1122)
+   (num-prs 85 1341))
+
+;; Part 2
+(num-prs 42686985 284100511221341)
